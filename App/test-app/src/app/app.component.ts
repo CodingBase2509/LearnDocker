5ab50cd28e40b9b63cf43ef643a100a6f7e5fed0
Fabian Dasler <f.dasler@smf.de>
2023-01-06 07:57:31 +0100

    add request to elasticsearch

--- App/test-app/src/app/app.component.ts
+++ App/test-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
@@ -13,7 +14,9 @@ export class AppComponent implements OnInit {
   isLoggedIn = false;
   userProfile: KeycloakProfile | null = null;
 
-  constructor(private keycloak: KeycloakService) {}
+  elastic_data: string = '';
+
+  constructor(private keycloak: KeycloakService, private http: HttpClient) {}
 
   public async ngOnInit(): Promise<void> {
     this.isLoggedIn = await this.keycloak.isLoggedIn();
@@ -21,6 +24,13 @@ export class AppComponent implements OnInit {
     if (this.isLoggedIn) {
       this.userProfile = await this.keycloak.loadUserProfile();
     }
+
+    this.http.get<string>('https://localhost:9200/profiles/_search', {
+      withCredentials: true,
+      headers: new HttpHeaders().append('username', 'elastic').append('password', 'test1234'),
+    }).subscribe(data => {
+      this.elastic_data = data;
+    });
   }
 
   public login() {
